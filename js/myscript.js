3114cec5468cd7ce3d14af48b38f654aacb551ea
Nik0-9 <filannino.nicola9@gmail.com>
2024-03-07 22:05:38 +0100

    refact

--- js/myscript.js
+++ js/myscript.js
@@ -2,25 +2,27 @@ let elNomeCognome = document.getElementById('NomeCognome');
 let elKm = document.getElementById('km');
 let elAge = document.getElementById('age');
 let elBtnCalcola = document.getElementById('calc');
-let elBtnAnnull = document.getElementById('anull');
+let elBtnReset = document.getElementById('reset');
 let elNome = document.getElementById('nome');
 let elOfferta = document.getElementById('sconto');
 let elPrezzo = document.getElementById('prezzo');
 const tarifBase = 0.21;
 let elVagone = document.getElementById('vagone');
 let elTicket = document.querySelector('.ticket');
+let elCPcode = document.getElementById('cp-code');
+let age = elAge.value;
 
 elBtnCalcola.addEventListener('click', function () {
     const nome = elNomeCognome.value;
     //  console.log(nome);
     const km = elKm.value;
     //  console.log(km);
-    const age = elAge.value;
     //  console.log(age);
     let priceBase = km * tarifBase;
     let price;
     let offerta;
     let vagone = getRndInteger(1 ,20);
+    let code = getRndInteger(1000, 9000);
     
 
     if (parseInt(age) < 18) {
@@ -31,21 +33,20 @@ elBtnCalcola.addEventListener('click', function () {
         offerta = `Sconto del 40%`;
     } else {
         price = priceBase;
-        offerta = `Nessuno sconto`;
+        offerta = `Nessuno sconto applicabile`;
     }
     elTicket.className = 'ticket';
     elNome.innerText = nome;
     elOfferta.innerText = offerta
-    elPrezzo.innerText = price.toFixed(2);
+    elPrezzo.innerText = price.toFixed(2) + '€';
     elVagone.innerText = vagone;
+    elCPcode.innerText = code;
 });
 
-elBtnAnnull.addEventListener('click', function () {
+elBtnReset.addEventListener('click', function () {
     //console.log('hai annullato');
     elTicket.className = `${'ticket'} ${'d-none'}`;
     document.getElementById('NomeCognome').value = '';
     document.getElementById('km').value = '';
-    document.getElementById('nome').innerHTML = '';
-    document.getElementById('sconto').innerHTML = '';
-    document.getElementById('prezzo').innerHTML = '';
+    document.getElementById('age').value = '';
 });
